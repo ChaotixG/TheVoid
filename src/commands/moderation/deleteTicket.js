35e4bcfed47701d21d1f338eacb168ad7b6cb1ad
ChaotixG <102786802+ChaotixG@users.noreply.github.com>
2025-06-08 06:44:28 -0400

    +Deleting posts and threads, -Bot ownership check

--- src/commands/moderation/deleteTicket.js
+++ src/commands/moderation/deleteTicket.js
@@ -5,32 +5,33 @@ module.exports = {
     description: 'Deletes the current ticket',
     callback: async (client, interaction) => {
         const modRoleName = 'mod';
-        const thread = interaction.channel;
+        const ticket = interaction.channel;
 
         // Check if the command is used in a thread
-        if (!thread.isThread()) {
+        if (!ticket.isThread()) {
             return interaction.reply({ content: '❌ This command can only be used inside a ticket.', flags: 64 });
         }
 
-        // Check if the user has the moderator role or admin permissions
+        // Check if the user has the moderator role, admin permissions or created the thread
         if (interaction.member.id !== interaction.guild.ownerId && 
+            !interaction.member.id === ticket.ownerId &&
             !interaction.member.roles.cache.some(role => role.name === modRoleName) && 
             !interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
-            return interaction.reply({ content: '❌ You do not have permission to delete this thread.', flags: 64 });
+            return interaction.reply({ content: '❌ You do not have permission to delete this ticket.', flags: 64 });
         }
 
-        // Check if the thread was created by the bot
-        if (thread.ownerId !== client.user.id) {
-            return interaction.reply({ content: '❌ This thread is not a ticket.', flags: 64 });
-        }
+        // Check if the ticket was created by the bot
+        /*if (ticket.ownerId !== client.user.id) {
+            return interaction.reply({ content: '❌ This ticket is not a ticket.', flags: 64 });
+        }*/
 
-        // Delete the thread
+        // Delete the ticket
         try {
-            await thread.delete();
-            console.log(`🗑️ Thread deleted by ${interaction.user.tag}: ${thread.name}`);
+            await ticket.delete();
+            console.log(`🗑️ ticket deleted by ${interaction.user.tag}: ${ticket.name}`);
         } catch (error) {
-            console.error('❌ Error deleting thread:', error);
-            return interaction.reply({ content: '❌ Failed to delete the thread.', flags: 64 });
+            console.error('❌ Error deleting ticket:', error);
+            return interaction.reply({ content: '❌ Failed to delete the ticket.', flags: 64 });
         }
     }
 };
